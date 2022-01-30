ed85886216715ca818fe6a4fdb7fab54b97055e2
Aidan Christie <aidanchristie98@gmail.com>
2022-01-30 03:16:51 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace PasswordGen
+{
+    class Program
+    {
+        static string generatePassword(int length)
+        {
+            const string validChars =
+                "abcdefghijklmnopqrstuvwxyz1234567890!.";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(validChars[rnd.Next(validChars.Length)]);
+            }
+            return res.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            string pass = generatePassword(length);
+            Console.WriteLine(pass);
+            Console.WriteLine("Press any button to exit!");
+            Console.ReadLine(); 
+        }
+
+    }
+}
